a099d4fd0f4465daa73f59dd48533fd591447c5f
Vladislav Surin <vlad_surin@mail.ru>
2023-08-07 16:51:24 +0300

    Filtering works with API

--- src/components/ProductFilter.vue
+++ src/components/ProductFilter.vue
@@ -64,7 +64,7 @@
               />
               <span
                 class="colors__value"
-                :style="`background-color: ${color.color}`"
+                :style="`background-color: ${color.code}`"
               >
               </span>
             </label>
@@ -178,8 +178,8 @@
 </template>
 
 <script>
-import categories from "@/data/categories";
-import colors from "@/data/colors";
+import axios from "axios";
+import { API_BASE_URL } from "@/config";
 
 export default {
   data() {
@@ -188,15 +188,17 @@ export default {
       currentPriceTo: 0,
       currentCategoryId: 0,
       currentColorId: 0,
+      categoriesData: null,
+      colorsData: null,
     };
   },
   props: ["priceFrom", "priceTo", "categoryId", "colorId"],
   computed: {
     categories() {
-      return categories;
+      return this.categoriesData ? this.categoriesData.items : [];
     },
     colors() {
-      return colors;
+      return this.colorsData ? this.colorsData.items : [];
     },
   },
   watch: {
@@ -226,6 +228,20 @@ export default {
       this.$emit("update:categoryId", 0);
       this.$emit("update:colorId", 0);
     },
+    loadCategories() {
+      axios
+        .get(`${API_BASE_URL}/api/productCategories`)
+        .then((response) => (this.categoriesData = response.data));
+    },
+    loadColors() {
+      axios
+        .get(`${API_BASE_URL}/api/colors`)
+        .then((response) => (this.colorsData = response.data));
+    },
+  },
+  created() {
+    this.loadCategories();
+    this.loadColors();
   },
 };
 </script>
