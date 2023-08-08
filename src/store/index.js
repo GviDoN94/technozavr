c58a30963c3091a027026deaf7b3f9c1ed352116
Vladislav Surin <vlad_surin@mail.ru>
2023-08-08 16:15:11 +0300

    Data from the products in the cart is loaded from the API

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,15 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import products from "@/data/products";
+import axios from "axios";
+import { API_BASE_URL } from "@/config";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
     cartProducts: [],
+    userAccessKey: null,
+    cartProductsData: [],
   },
   mutations: {
     addProductToCart(state, { productId, amount }) {
@@ -37,13 +40,33 @@ export default new Vuex.Store({
         (item) => item.productId !== productId
       );
     },
+    updateUserAccessKey(state, accessKey) {
+      state.userAccessKey = accessKey;
+    },
+    updateCartProductsData(state, items) {
+      state.cartProductsData = items;
+    },
+    syncCartProducts(state) {
+      state.cartProducts = state.cartProductsData.map((item) => {
+        return {
+          productId: item.product.id,
+          amount: item.quantity,
+        };
+      });
+    },
   },
   getters: {
     cartDetailProducts(state) {
       return state.cartProducts.map((item) => {
+        const product = state.cartProductsData.find(
+          (p) => p.product.id === item.productId
+        ).product;
         return {
           ...item,
-          product: products.find((p) => p.id === item.productId),
+          product: {
+            ...product,
+            image: product.image.file.url,
+          },
         };
       });
     },
@@ -52,6 +75,24 @@ export default new Vuex.Store({
         (acc, item) => item.product.price * item.amount + acc,
         0
       );
+    },
+  },
+  actions: {
+    loadCart(context) {
+      axios
+        .get(`${API_BASE_URL}/api/baskets`, {
+          params: {
+            userAccessKey: context.state.userAccessKey,
+          },
+        })
+        .then((response) => {
+          if (context.state.userAccessKey) {
+            localStorage.setItem("userAccessKey", response.data.user.accessKey);
+            context.commit("updateUserAccessKey", response.data.user.accessKey);
+          }
+          context.commit("updateCartProductsData", response.data.items);
+          context.commit("syncCartProducts");
+        });
     },
   },
 });
